6e5f5a75a60d2bd25c96297da0af69a572c5f2ca
muizbello <bellomuiz78+1@gmail.com>
2024-05-25 21:07:23 +0000

    Text style created and Increased to 24

--- MyApp/App.js
+++ MyApp/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My Name is YourName!</Text>
+      <Text style ={styles.text}>My Name is Muiz!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#ff0000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  text: {
+    fontSize: 24,
   },
 });
