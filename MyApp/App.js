ac3a68eb40754620f74a88ff1ed7ea6579806b22
muizbello <bellomuiz78+1@gmail.com>
2024-05-25 20:56:01 +0000

    Text Component Changed

--- MyApp/App.js
+++ MyApp/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Openx up App.js to start working on your app!</Text>
+      <Text>My Name is YourName!</Text>
       <StatusBar style="auto" />
     </View>
   );
